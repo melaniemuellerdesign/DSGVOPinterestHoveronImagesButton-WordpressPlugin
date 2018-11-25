7b94fa5466584885851f07f7bb06747be650f0ab
Fidentia <43379415+Fidentia@users.noreply.github.com>
2018-11-25 11:26:41 +0100

    Relative path of Pin
    Add a realtive path of PinButton

--- js/dsgvopinteresthover.js
+++ js/dsgvopinteresthover.js
@@ -1,6 +1,9 @@
-jQuery(function() {
-    jQuery ("#content img").before("<div class=mysharecontainer><div class=mysharebutton>");
+jQuery(document).ready(function($) {
+	
+	var pin= window.location.protocol + "//" + window.location.host + "/wp-content/plugins/jQueryDSGVOPinterestButton/images/simple_icons/Pinterest.png";
+		
+	jQuery ("#content img").before("<div class=mysharecontainer><div class=mysharebutton>");
 	jQuery ("#content img").after("</div></div>");
- 	jQuery (".mysharebutton").append('<ul class="share-buttons"><li><a class="share" href="http://pinterest.com/pin/create/button/?url=&description=" target="_blank" title="Pin it" onclick="window.open(\'http://pinterest.com/pin/create/button/?url=\' + encodeURIComponent(document.URL) + \'&description=\' +  encodeURIComponent(document.title)); return false;"><img class="pinmenow" alt="Pin it" src="https://meterlimit.de/wp-content/plugins/jQueryDSGVOPinterestButton/images/simple_icons/Pinterest.png" /></a></li></ul>');
-
+ 	jQuery (".mysharebutton").append('<ul class="share-buttons"><li><a class="share" href="http://pinterest.com/pin/create/button/?url=&description=" target="_blank" title="Pin it" onclick="window.open(\'http://pinterest.com/pin/create/button/?url=\' + encodeURIComponent(document.URL) + \'&description=\' +  encodeURIComponent(document.title)); return false;"><img class="pinmenow" alt="Pin it" src= "' + pin + '" /></a></li></ul>');
+	
 });
