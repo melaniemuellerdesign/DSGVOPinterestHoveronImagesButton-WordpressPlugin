b7b4401f337327eb5d14713af39300db6cced970
Fidentia <43379415+Fidentia@users.noreply.github.com>
2019-01-19 17:01:48 +0100

    Update dsgvopinteresthover.js

--- js/dsgvopinteresthover.js
+++ js/dsgvopinteresthover.js
@@ -1,9 +1,9 @@
 jQuery(document).ready(function($) {
-	
-	var pin= window.location.protocol + "//" + window.location.host + "/wp-content/plugins/jQueryDSGVOPinterestButton/images/simple_icons/Pinterest.png";
-		
+
+	var pin= window.location.protocol + "//" + window.location.host + "/wp-content/plugins/DSGVOPinterestHoveronImagesButton/images/simple_icons/Pinterest.svg";
+
 	jQuery ("#content img").before("<div class=mysharecontainer><div class=mysharebutton>");
 	jQuery ("#content img").after("</div></div>");
  	jQuery (".mysharebutton").append('<ul class="share-buttons"><li><a class="share" href="http://pinterest.com/pin/create/button/?url=&description=" target="_blank" title="Pin it" onclick="window.open(\'http://pinterest.com/pin/create/button/?url=\' + encodeURIComponent(document.URL) + \'&description=\' +  encodeURIComponent(document.title)); return false;"><img class="pinmenow" alt="Pin it" src= "' + pin + '" /></a></li></ul>');
-	
+
 });
