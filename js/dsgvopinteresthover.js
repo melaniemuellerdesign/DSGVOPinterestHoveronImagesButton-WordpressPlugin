156aefe776ef5101c1e5134c383e5791ce0d7379
Melanie Müller <43379415+codearosa@users.noreply.github.com>
2021-07-25 17:13:18 +0200

    change path

--- js/dsgvopinteresthover.js
+++ js/dsgvopinteresthover.js
@@ -1,9 +1,9 @@
 jQuery(document).ready(function($) {
 
-	var pin= window.location.protocol + "//" + window.location.host + "/wp-content/plugins/DSGVOPinterestHoveronImagesButton/images/simple_icons/Pinterest.png";
+	var pin = window.location.protocol + "//" + window.location.host + "/wp-content/plugins/DSGVOPinterestHoveronImagesButton-WordpressPlugin/images/simple_icons/Pinterest.png";
 
-	jQuery ("#content img").before("<div class=mysharecontainer><div class=mysharebutton>");
-	jQuery ("#content img").after("</div></div>");
+	jQuery (".mmu-pin-it img").before("<div class=mysharecontainer><div class=mysharebutton>");
+	jQuery (".mmu-pin-it img").after("</div></div>");
  	jQuery (".mysharebutton").append('<ul class="share-buttons"><li><a class="share" href="http://pinterest.com/pin/create/button/?url=&description=" target="_blank" title="Pin it" onclick="window.open(\'http://pinterest.com/pin/create/button/?url=\' + encodeURIComponent(document.URL) + \'&description=\' +  encodeURIComponent(document.title)); return false;"><img class="pinmenow" alt="Pin it" src= "' + pin + '" /></a></li></ul>');
 
 });
