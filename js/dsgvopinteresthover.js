6e59a534eb43011bfcb5ecea6d13c3bee401fc61
Melanie Müller <43379415+melaniemuellerdesign@users.noreply.github.com>
2021-08-22 20:04:25 +0200

    Update dsgvopinteresthover.js

--- js/dsgvopinteresthover.js
+++ js/dsgvopinteresthover.js
@@ -4,6 +4,6 @@ jQuery(document).ready(function($) {
 
 	jQuery (".mmu-pin-it img").before("<div class=mysharecontainer><div class=mysharebutton>");
 	jQuery (".mmu-pin-it img").after("</div></div>");
- 	jQuery (".mysharebutton").append('<ul class="share-buttons"><li><a class="share" href="http://pinterest.com/pin/create/button/?url=&description=" target="_blank" title="Pin it" onclick="window.open(\'http://pinterest.com/pin/create/button/?url=\' + encodeURIComponent(document.URL) + \'&description=\' +  encodeURIComponent(document.title)); return false;"><img class="pinmenow" alt="Pin it" src= "' + pin + '" /></a></li></ul>');
+ 	jQuery (".mysharebutton").append('<ul class="share-buttons"><li><a rel="noopener" class="share" href="http://pinterest.com/pin/create/button/?url=&description=" target="_blank" title="Pin it" onclick="window.open(\'http://pinterest.com/pin/create/button/?url=\' + encodeURIComponent(document.URL) + \'&description=\' +  encodeURIComponent(document.title)); return false;"><img class="pinmenow" alt="Pin it" src= "' + pin + '" /></a></li></ul>');
 
 });
